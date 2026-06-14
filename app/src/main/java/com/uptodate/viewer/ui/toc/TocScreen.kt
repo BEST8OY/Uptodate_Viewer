f8e95d4bbdd7832779ffb57a145910225b31ed8f
best8oy <alireza.bestboy@outlook.com>
2026-06-14 04:06:49 +0330

    fix: add missing clickable import

--- app/src/main/java/com/uptodate/viewer/ui/toc/TocScreen.kt
+++ app/src/main/java/com/uptodate/viewer/ui/toc/TocScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
