a24936ff763840a9dd2373c411da51a3971df4e4
best8oy <alireza.bestboy@outlook.com>
2026-06-14 22:18:42 +0330

    sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.uptodate.viewer"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.uptodate.viewer"
         minSdk = 31
-        targetSdk = 35
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
     }
